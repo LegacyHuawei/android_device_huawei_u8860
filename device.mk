5a07c78e4aa04023504eb497e6f5eeef72e405d7
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-12-30 12:37:09 +0200

    u8860: Consolidate media_profiles.xml

--- device.mk
+++ device.mk
@@ -38,10 +38,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.target.rc:root/init.target.rc
 
-# Configs
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
-
 # Additional packages
 PRODUCT_PACKAGES += \
 	libnetcmdiface
